abb99e3ebebfcbdb81625399312650a218081136
yummygoods <melissa@yummygoods.com>
2023-09-03 12:26:34 -0400

    comments for what to do next time

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+//form
+//include a header + footer
+//event in sql with 3 fields
+//connect db
+//show event title in header
+//save user input in db
+//confirm page, "thank you for signing up,  '$personName'"
+//here's a list of the people attending 
+
+>
